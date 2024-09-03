a00a9766786c879b72ebc0cbf389e7c5fac93be3
Kunal More <kunalmore2288@gmail.com>
2024-09-03 18:43:28 -0400

    for space between each list item.

---
+++ app/src/main/java/com/example/to_do_list/SpaceItemDecoration.java
@@ -0,0 +1,24 @@
+package com.example.to_do_list;
+
+import android.graphics.Rect;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class SpaceItemDecoration extends RecyclerView.ItemDecoration {
+
+    private final int spaceHeight;
+
+    public SpaceItemDecoration(int spaceHeight) {
+        this.spaceHeight = spaceHeight;
+    }
+
+    @Override
+    public void getItemOffsets(@NonNull Rect outRect, @NonNull View view, @NonNull RecyclerView parent, @NonNull RecyclerView.State state) {
+        outRect.bottom = spaceHeight;
+
+        // If you want space at the top of the first item or other specific cases, you can add conditions here.
+    }
+}
+
