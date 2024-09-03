bfff80632bf28201b0d06229805226f32d08f686
Kunal More <kunalmore2288@gmail.com>
2024-09-03 18:51:49 -0400

    Database file to store both List table and Task table.

---
+++ app/src/main/java/com/example/to_do_list/ToDoRoomDatabase.java
@@ -0,0 +1,41 @@
+package com.example.to_do_list;
+
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+@Database(entities = {ListEntity.class,TaskEntity.class},version = 2,exportSchema = false)
+public abstract class ToDoRoomDatabase extends RoomDatabase {
+
+    public abstract ListDao listDao();
+    public abstract TaskDao taskDao();
+
+    private static volatile ToDoRoomDatabase INSTANCE;
+    private static final int NUMBER_OF_THREADS = 4;
+
+    static final ExecutorService databaseWriteExecutor = Executors.newFixedThreadPool(NUMBER_OF_THREADS);
+
+    static ToDoRoomDatabase getDatabase(final Context context){
+        if (INSTANCE == null) {
+            synchronized (ToDoRoomDatabase.class) {
+                if (INSTANCE == null) {
+                    INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
+                                    ToDoRoomDatabase.class, "todo_database")
+                            .build();
+                }
+            }
+        }
+        return INSTANCE;
+    }
+
+    public static ExecutorService getDatabaseWriteExecutor() {
+        return databaseWriteExecutor;
+    }
+
+
+}
