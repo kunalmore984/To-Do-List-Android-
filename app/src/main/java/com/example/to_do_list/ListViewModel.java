a23a0bb13058defd7bbda79e3cd13473c1474b8e
Kunal More <kunalmore2288@gmail.com>
2024-09-03 18:38:19 -0400

    Recycler view model file for main list.

---
+++ app/src/main/java/com/example/to_do_list/ListViewModel.java
@@ -0,0 +1,33 @@
+package com.example.to_do_list;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+import java.util.List;
+
+public class ListViewModel extends AndroidViewModel {
+
+    private ToDoRepository mToDoRepository;
+    private final LiveData<List<ListEntity>> mAllList;
+
+    public ListViewModel(Application application){
+        super(application);
+        mToDoRepository =new ToDoRepository(application);
+        mAllList = mToDoRepository.getAllWords();
+    }
+
+    LiveData<List<ListEntity>> getmAllList() {
+        return mAllList;
+    }
+
+    public void insert(ListEntity listEntity){
+        mToDoRepository.insert(listEntity);
+    }
+
+    public void delete(ListEntity listEntity) {
+        mToDoRepository.delete(listEntity);
+    }
+}
